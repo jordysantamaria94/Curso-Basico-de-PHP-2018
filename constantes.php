b761b32989b56f38f6949dacafd0f76bc4d622db
jordysantamaria94 <santmjoy@gmail.com>
2018-10-21 22:53:33 -0500

    Primeros archivos del curso

---
+++ constantes.php
@@ -0,0 +1,6 @@
+<?php
+	define("CONSTANTE_EJEMPLO", 44);
+	define("NOMBRE_MASCOTA", "Niebla");
+
+	echo "El nombre de tu mascota es: ".CONSTANTE_EJEMPLO;
+?>
---
+++ variables.php
@@ -0,0 +1,20 @@
+<?php 
+	$algun_nombre = "Hola";
+	$Algun_nombre = "Mundo";
+
+	//echo $algun_nombre ." ". $Algun_nombre;
+
+	$_4Cuatro = "Numero";
+
+	//echo $_4Cuatro;
+
+	$cadena = "Soy una cadena";
+	$numero = 4;
+	$flotante = 4.4;
+	$caracter = 'C';
+	$boolean = true;
+
+	var_dump($caracter);
+
+	//echo $boolean;
+?>
